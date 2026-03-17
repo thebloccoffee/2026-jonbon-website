4833a30b77b9f570faee84aedb7bac98bea0d904
Jon Bon Tabas <jonbon@Dell-3.local>
2026-03-16 20:54:14 -0700

    Fix Vercel routing: serve all files through express.static

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname)));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 const PRODUCTS = {
   lut_pack: {
     name: 'Cinematic LUT Pack',
